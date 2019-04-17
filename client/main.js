747b4171fc1665dfe38b7c3be2f885a91980ca30
alex kochetov <aleksandrko@wix.com>
2019-04-18 00:49:06 +0300

    Part 2: Draw our first system message

--- client/main.js
+++ client/main.js
@@ -1,11 +1,14 @@
 import {UserName} from './modules/user-name.js';
 import {Socket} from './modules/socket.js';
+import {Messages} from './modules/messages.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const socket = new Socket();
   const userName = new UserName('#username');
+  const messages = new Messages('#messages');
 
   socket.onNameAssigned(username => {
     userName.render(username);
+    messages.appendSystem(`<b>${username}</b> assigned to you.`);
   });
 });
---
+++ client/modules/messages.js
@@ -0,0 +1,13 @@
+export class Messages {
+  constructor(selector) {
+    this.node = document.querySelector(selector);
+  }
+
+  append(username, message) {
+    this.node.innerHTML += `[${username}] ${message}\n`;
+  }
+
+  appendSystem(message) {
+    this.append('system', message);
+  }
+}
